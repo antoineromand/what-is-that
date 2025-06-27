8df55e10b5e5ac7c1899a2a517fd82f25c590a6e
Antoine Romand <antoine.romand@epitech.eu>
2025-06-27 18:35:44 +0200

    feat: should failed and not authorize the merged pr

--- ci-cd-example-1/src/test/java/com/ci_cd_example/test/TestServiceUnitTest.java
+++ ci-cd-example-1/src/test/java/com/ci_cd_example/test/TestServiceUnitTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.api.Test;
 
 public class TestServiceUnitTest {
 
-    private final TestService testService = new TestService();
-
-
-    @Test
-    public void shouldReturnParameterInString() {
-        int parameter = 3;
-        String expectedResult = "3";
-
-        String result = this.testService.convertToString(parameter);
-        Assertions.assertNotEquals("3   ", result);
-        Assertions.assertEquals(expectedResult, result);
-    }
+//    private final TestService testService = new TestService();
+//
+//
+//    @Test
+//    public void shouldReturnParameterInString() {
+//        int parameter = 3;
+//        String expectedResult = "3";
+//
+//        String result = this.testService.convertToString(parameter);
+//        Assertions.assertNotEquals("3   ", result);
+//        Assertions.assertEquals(expectedResult, result);
+//    }
 
 }
